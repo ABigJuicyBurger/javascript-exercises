35df10b0363447bdbb00f2de25325d2b2c1af72b
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-06-30 18:05:59 -0600

    understanding fibonacci coding

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  member = parseInt(member);
 
+  if (member === 0) {
+    return 0;
+  }
+  if (member === 1 || member === 2) {
+    return 1;
+  }
+  if (typeof member !== "number" || member < 0) {
+    return "OOPS";
+  }
+
+  let prev1 = 1;
+  let prev2 = 1;
+  let current = 0;
+
+  for (let i = 3; i <= member; i++) {
+    current = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = current;
+  }
+  return prev1;
 };
 
 // Do not edit below this line
